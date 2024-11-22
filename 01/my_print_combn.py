038046bd85019583207b2dfd3c6d1b143aae6608
JuanMaAl <juanmaal1986@gmail.com>
2024-11-22 15:48:36 +0100

    my_print_combn second try not completed yet, print the numbers with the correct zeros but print all numbers

--- 01/my_print_combn.py
+++ 01/my_print_combn.py
@@ -1,40 +1,35 @@
 from my_putchar import *
+from my_put_nbr import *
 
-def print_one_digit(n = -1):
-	digit = "0123456789"
+def limit(n):
+	limit = 1
+	while (n > 0):
+		limit *= 10
+		n -= 1
+	return limit
+
+def put_zeros(i, n):
+	if n == 1 and i == 0:
+		my_putchar("0")
+		return 
+	j = i
+	k = 1
+	while (j // 10 > 0):
+		k += 1
+		j //= 10
+	n_zeros = n - k
+	while (n_zeros > 0):
+		my_putchar("0")
+		n_zeros -= 1
+
+
+def my_print_combn(n):
+	lim = limit(n)
 	i = 0
-	while i < 10:
-		if (n >= 0):
-			my_putchar(digit[n])
-		my_putchar(digit[i])
+	while (i < lim):
+		put_zeros(i, n)
+		my_put_nbr(i)
+		my_putchar(" ")
 		i += 1
 
-def print_two_digits(n = -1):
-	digit = "0123456789"
-	i = 0
-	while i < 10:
-		if (n >= 0):
-			my_putchar(digit[n])
-		print_one_digit(i)
-		i += 1
-
-def my_print_comb(n):
-	if n > 10:
-		n = 10
-	if n < 0:
-		n = 0
-	digit = "0123456789"
-	digit_arr = [] 
-	i = n
-	j = 0
-	while i > 0:
-		digit_arr.append(j)
-		j += 1
-		i -= 1
-	i = 0
-	while i < n:
-		my_putchar(digit[digit_arr[i]])
-		i += 1
-	my_putchar("\n")
-	
-print_two_digits()		
+my_print_combn(9)
