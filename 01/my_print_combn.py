d1cc596679459645cfea933da8064e0a479ddb8c
JuanMaAl <juanmaal1986@gmail.com>
2024-11-22 11:41:25 +0100

    functions print_one_digit and print_two_digits

--- 01/my_print_combn.py
+++ 01/my_print_combn.py
@@ -1,5 +1,23 @@
 from my_putchar import *
 
+def print_one_digit(n = -1):
+	digit = "0123456789"
+	i = 0
+	while i < 10:
+		if (n >= 0):
+			my_putchar(digit[n])
+		my_putchar(digit[i])
+		i += 1
+
+def print_two_digits(n = -1):
+	digit = "0123456789"
+	i = 0
+	while i < 10:
+		if (n >= 0):
+			my_putchar(digit[n])
+		print_one_digit(i)
+		i += 1
+
 def my_print_comb(n):
 	if n > 10:
 		n = 10
@@ -19,8 +37,4 @@ def my_print_comb(n):
 		i += 1
 	my_putchar("\n")
 	
-my_print_comb(1)		
-my_print_comb(3)		
-my_print_comb(7)		
-my_print_comb(9)		
-my_print_comb(10)		
+print_two_digits()		
