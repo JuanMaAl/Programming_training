0d5bbd83a444eda4c5941e54caf594f4fa9e66fe
JuanMaAl <juanmaal1986@gmail.com>
2025-03-25 23:04:03 +0100

    Easy Exercises 03

---
+++ 30_easy/03/03.py
@@ -0,0 +1,8 @@
+# 3. Find the Largest Element in a List
+
+def find_largest(source):
+	largest = max(source)
+	return largest
+
+animales = ['leon', 'gato', 'elefante', 'rinoceronte', 'murcielago', 'oso']
+print(find_largest(animales))
