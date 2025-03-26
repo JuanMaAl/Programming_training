2548090aa9e3e00ba4fb8db15564db4f17bbed34
JuanMaAl <juanmaal1986@gmail.com>
2025-03-26 09:11:56 +0100

    Easy Exercise 08

---
+++ 30_easy/08/08.py
@@ -0,0 +1,16 @@
+# 8.Display Numbers Divisible by Five
+
+def divisible_by_five(lst):
+	divisibles_by_five = []
+	for number in lst:
+		if number % 5 == 0:
+			divisibles_by_five.append(number)
+	return divisibles_by_five
+
+numbers_lst = []
+for number in range(100):
+	number += 1
+	numbers_lst.append(number)
+print(numbers_lst)
+print()
+print(divisible_by_five(numbers_lst))
