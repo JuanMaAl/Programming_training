92d6cef63be294b01580c0c1d9edcbe25173cb93
JuanMaAl <juanmaal1986@gmail.com>
2025-05-16 02:29:47 +0200

    Add and assert str type

--- GoogleAutomation/Course2Module5/raising_errors/errors2.py
+++ GoogleAutomation/Course2Module5/raising_errors/errors2.py
@@ -2,7 +2,7 @@ from validations import validate_user
 
 print (validate_user("", 1))
 print(validate_user("myuser", 1))
-# print(validate_user(88, 1))
+print(validate_user(88, 1))
 print(validate_user([], 1))
-# print(validate_user(["name"], 1))
-# print(validate_user([3], 1))
+print(validate_user(["name"], 1))
+print(validate_user([3], 1))
