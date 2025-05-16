2d939e118a064a049026d3b182d5622d37e1ef5f
JuanMaAl <juanmaal1986@gmail.com>
2025-05-17 00:31:28 +0200

    Add line variable to echo separator line

--- GoogleAutomation/Course2Module6/creating_bash_script/gather-information.sh
+++ GoogleAutomation/Course2Module6/creating_bash_script/gather-information.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo "Starting at: $(date)"; echo
+line="-----------------------------"
+echo "Starting at: $(date)"; echo $line
 
-echo "UPTIME"; uptime; echo
+echo "UPTIME"; uptime; echo $line
 
-echo "FREE"; free; echo
+echo "FREE"; free; echo $line
 
-echo "WHO"; who; echo
+echo "WHO"; who; echo $line
 
 echo "Finishing at: $(date)"
