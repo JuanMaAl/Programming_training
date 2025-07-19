4a05cda7f0c56a07e37da55499fa16cce990373f
JuanMaAl <juanmaal1986@gmail.com>
2025-07-19 06:24:19 +0200

    attach a png image to the email message

--- GoogleAutomation/automating-real-world-tasks/module03/email_message.py
+++ GoogleAutomation/automating-real-world-tasks/module03/email_message.py
@@ -1,7 +1,8 @@
 from email.message import EmailMessage
+import os.path
+import mimetypes
 
 message = EmailMessage()
-
 sender = "me@example.com"
 recipient = "you@example.com"
 message['From'] = sender
@@ -12,6 +13,15 @@ body = """Hey there!
 I'm learning to send emails using Python!"""
 message.set_content(body)
 
+attachment_path = "./example.png"
+attachment_filename = os.path.basename(attachment_path)
+mime_type, _ = mimetypes.guess_type(attachment_path)
+mime_type, mime_subtype = mime_type.split('/', 1)
+
+with open(attachment_path, 'rb') as ap:
+	message.add_attachment(ap.read(),
+							maintype = mime_type,
+							subtype = mime_subtype,
+							filename = os.path.basename(attachment_path))
+
 print(message)
-
-
