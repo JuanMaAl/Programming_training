f246963a09a09f06893639a637b5fa2cbf8ce143
JuanMaAl <juanmaal1986@gmail.com>
2025-07-20 23:10:32 +0200

    Adding a graphic to the generated pdf

--- GoogleAutomation/automating-real-world-tasks/module03/generating-report-pdf.py
+++ GoogleAutomation/automating-real-world-tasks/module03/generating-report-pdf.py
@@ -2,6 +2,9 @@ from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.piecharts import Pie
+from reportlab.lib.units import inch
 
 fruit = {
 	"elderberries": 1,
@@ -23,4 +26,17 @@ for fruit_name, number_fruits in fruit.items():
 
 table_style = [('GRID', (0,0), (-1,-1), 1, colors.black)]
 report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
-report.build([report_title, report_table])
+
+report_pie = Pie(width=3*inch, height=3*inch)
+report_pie.data = []
+report_pie.labels = []
+for fruit_name in sorted(fruit):
+	report_pie.data.append(fruit[fruit_name])
+	report_pie.labels.append(fruit_name)
+
+report_chart = Drawing()
+report_chart.add(report_pie)
+
+
+report.build([report_title, report_table, report_chart])
+
