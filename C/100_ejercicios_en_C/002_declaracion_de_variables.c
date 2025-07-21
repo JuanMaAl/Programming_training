f4202686103ff74d1e2c4dab6f2f0a8302d42980
JuanMaAl <juanmaal1986@gmail.com>
2025-07-22 00:14:46 +0200

    002 declaración de variables

---
+++ C/100_ejercicios_en_C/002_declaracion_de_variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	// Declaración de diferentes tipos de variables
+	int entero = 10;
+	float flotante = 10.5;
+	double doble = 20.123456;
+	char caracter = 'A';
+
+	// Mostrar los valores de las variables
+	printf("Valor entero: %d\n", entero);
+	printf("Valor flotante: %f\n", flotante);
+	printf("Valor doble: %lf\n", doble);
+	printf("Caracter: %c\n", caracter);
+
+	return 0;
+}
